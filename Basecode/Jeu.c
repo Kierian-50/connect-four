c00a0e572f25e4dfe4bfa067982ca339552ee3c8
0xARK <castella.mateo@gmail.com>
2022-01-16 11:24:39 +0100

    graphical interface works with player vs player mode

--- Basecode/Jeu.c
+++ Basecode/Jeu.c
@@ -335,48 +335,32 @@ int boucleGraphique(Partie *partie) {
     int grid_width = 7;
     int grid_height = 6;
 
-    // + 1 so that the last grid lines fit in the screen.
     int window_width = (grid_width * grid_cell_size) + 1;
     int window_height = (grid_height * grid_cell_size) + 1;
 
-    // Place the grid cursor in the middle of the screen.
+    // Create the current pawn to play without draw it on the grid
+    SDL_Rect pawn = {0,0,grid_cell_size - 1,grid_cell_size - 1,};
 
-    SDL_Rect pawn = {
-            .x = 0,
-            .y = 0,
-            .w = grid_cell_size - 1,
-            .h = grid_cell_size - 1,
-    };
+    // Create a mouse hover cursor
+    SDL_Rect hover = {pawn.x, pawn.y, grid_cell_size,grid_cell_size};
 
-    // The cursor ghost is a cursor that always shows in the cell below the
-    // mouse cursor.
-    SDL_Rect grid_cursor_ghost = {pawn.x, pawn.y, grid_cell_size,grid_cell_size};
-
-    // Dark theme.
-    SDL_Color grid_background = {22, 22, 22, 255}; // Barely Black
+    SDL_Color grid_background_color = {22, 22, 22, 255};
     SDL_Color grid_line_color = {44, 44, 44, 255}; // Dark grey
-    SDL_Color grid_cursor_ghost_color = {44, 44, 44, 255};
-    SDL_Color p1_color = {255, 0, 0, 255}; // Red
-    SDL_Color p2_color = {255, 255, 0, 255}; // Yellow
-
-    // Light Theme.
-    // SDL_Color grid_background = {233, 233, 233, 255}; // Barely white
-    // SDL_Color grid_line_color = {200, 200, 200, 255}; // Very light grey
-    // SDL_Color grid_cursor_ghost_color = {200, 200, 200, 255};
-    // SDL_Color grid_cursor_color = {160, 160, 160, 255}; // Grey
+    SDL_Color grid_hover_color = {44, 44, 44, 255};
+    // Red
+    SDL_Color p1_color = {255, 0, 0, 255};
+    // Yellow
+    SDL_Color p2_color = {255, 255, 0, 255};
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Initialize SDL: %s",
-                     SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Initialize SDL: %s",SDL_GetError());
         return EXIT_FAILURE;
     }
 
     SDL_Window *window;
     SDL_Renderer *renderer;
-    if (SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window,
-                                    &renderer) < 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                     "Create window and renderer: %s", SDL_GetError());
+    if (SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window,&renderer) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,"Create window and renderer: %s", SDL_GetError());
         return EXIT_FAILURE;
     }
 
@@ -386,56 +370,55 @@ int boucleGraphique(Partie *partie) {
     SDL_bool mouse_active = SDL_FALSE;
     SDL_bool mouse_hover = SDL_FALSE;
 
-    while (!quit) {
+    Etat etat = calculerEtat(partie);
+
+    while (!quit && etat == EN_COURS) {
         SDL_Event event;
+        int coup = 0;
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_MOUSEBUTTONDOWN:
-                    jouerCoup(partie, event.motion.x/grid_cell_size + 1);
+                    // Place a new pawn at the clicked case.
+                    coup = jouerCoup(partie, event.motion.x/grid_cell_size + 1);
                     break;
                 case SDL_MOUSEMOTION:
-                    grid_cursor_ghost.x = (event.motion.x / grid_cell_size) * grid_cell_size;
-                    grid_cursor_ghost.y = (event.motion.y / grid_cell_size) * grid_cell_size;
-
+                    // Select current case on which we will ad an hover effect
+                    hover.x = (event.motion.x / grid_cell_size) * grid_cell_size;
+                    hover.y = (event.motion.y / grid_cell_size) * grid_cell_size;
                     if (!mouse_active)
                         mouse_active = SDL_TRUE;
                     break;
                 case SDL_WINDOWEVENT:
+                    // Set hover to true
                     if (event.window.event == SDL_WINDOWEVENT_ENTER && !mouse_hover)
                         mouse_hover = SDL_TRUE;
                     else if (event.window.event == SDL_WINDOWEVENT_LEAVE && mouse_hover)
                         mouse_hover = SDL_FALSE;
                     break;
                 case SDL_QUIT:
+                    // Quit graphical interface
                     quit = SDL_TRUE;
                     break;
             }
         }
         // Draw grid background.
-        SDL_SetRenderDrawColor(renderer, grid_background.r, grid_background.g,grid_background.b, grid_background.a);
+        SDL_SetRenderDrawColor(renderer, grid_background_color.r, grid_background_color.g,grid_background_color.b, grid_background_color.a);
         SDL_RenderClear(renderer);
 
         // Draw grid lines.
         SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,grid_line_color.b, grid_line_color.a);
-
-        for (int x = 0; x < 1 + grid_width * grid_cell_size; x += grid_cell_size) {
+        for (int x = 0; x < 1 + grid_width * grid_cell_size; x += grid_cell_size)
             SDL_RenderDrawLine(renderer, x, 0, x, window_height);
-        }
-
-        for (int y = 0; y < 1 + grid_height * grid_cell_size; y += grid_cell_size) {
+        for (int y = 0; y < 1 + grid_height * grid_cell_size; y += grid_cell_size)
             SDL_RenderDrawLine(renderer, 0, y, window_width, y);
-        }
 
-        // Draw grid ghost cursor.
+        // Draw grid hover cursor.
         if (mouse_active && mouse_hover) {
-            SDL_SetRenderDrawColor(renderer, grid_cursor_ghost_color.r,
-                                   grid_cursor_ghost_color.g,
-                                   grid_cursor_ghost_color.b,
-                                   grid_cursor_ghost_color.a);
-            SDL_RenderFillRect(renderer, &grid_cursor_ghost);
+            SDL_SetRenderDrawColor(renderer, grid_hover_color.r,grid_hover_color.g,grid_hover_color.b,grid_hover_color.a);
+            SDL_RenderFillRect(renderer, &hover);
         }
 
-        // Draw grid cursor.
+        // Draw grid pawns.
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
                 if (partie->plateau[i][j] == J1) {
@@ -452,7 +435,11 @@ int boucleGraphique(Partie *partie) {
             }
         }
 
-        partie->tour = partie->tour == J1 ? J2 : J1;
+        // If new pawn is placed, change player
+        if (coup) {
+            etat = calculerEtat(partie);
+            partie->tour = partie->tour == J1 ? J2 : J1;
+        }
 
         SDL_RenderPresent(renderer);
     }
